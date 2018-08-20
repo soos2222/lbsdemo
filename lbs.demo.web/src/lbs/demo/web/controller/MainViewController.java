0b2db65422cbec96568c686daa02fdce337e34f8
Lenovo <Lenovo@DESKTOP-670EF3R>
2018-08-20 17:03:34 +0900

    person 몽고디비 테스트

--- lbs.demo.web/src/lbs/demo/web/controller/MainViewController.java
+++ lbs.demo.web/src/lbs/demo/web/controller/MainViewController.java
@@ -1,15 +1,20 @@
 package lbs.demo.web.controller;
 
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import lbs.demo.web.bean.PersonBean;
+import lbs.demo.web.service.iface.PersonServiceInterface;
 
 @Controller
 public class MainViewController {
+
+	@Autowired private PersonServiceInterface service;
 
 	@RequestMapping(value = "navermap.html", method=RequestMethod.GET)
 	public ModelAndView viewNaverMapMain(HttpServletRequest request, HttpServletResponse response){
@@ -29,9 +34,55 @@ public class MainViewController {
 
 	@RequestMapping(value = "test.html", method=RequestMethod.GET)
 	public ModelAndView testMain(HttpServletRequest request, HttpServletResponse response){
-		ModelAndView view = new ModelAndView("/index2");
+		ModelAndView view = new ModelAndView("/person");
+
+		List<PersonBean> persons = service.listPerson(request);
+		view.addObject("persons", persons);
+		return view;
+
+	}
+
+	@RequestMapping(value = "addperson.html", method=RequestMethod.POST)
+	public ModelAndView addPerson(HttpServletRequest request, HttpServletResponse response, PersonBean person){
+		ModelAndView view = new ModelAndView("/person");
+
+		service.addPerson(person, request);
+		List<PersonBean> persons = service.listPerson(request);
+		view.addObject("persons", persons);
 
 		return view;
 
+	}
+
+	@RequestMapping(value = "deleteperson.html", method=RequestMethod.POST)
+	public ModelAndView deletePerson(HttpServletRequest request, HttpServletResponse response, PersonBean person){
+		ModelAndView view = new ModelAndView("/person");
+
+		service.deletePerson(person, request);
+		List<PersonBean> persons = service.listPerson(request);
+		view.addObject("persons", persons);
+
+		return view;
+
+	}
+
+	@RequestMapping(value = "editperson.html", method=RequestMethod.POST)
+	public ModelAndView editPerson(HttpServletRequest request, HttpServletResponse response, PersonBean person){
+		ModelAndView view = new ModelAndView("/person");
+
+		service.updatePerson(person, request);
+		List<PersonBean> persons = service.listPerson(request);
+		view.addObject("persons", persons);
+
+		return view;
+
+	}
+
+
+	@RequestMapping(value = "text.html", method=RequestMethod.GET)
+	public ModelAndView textTest(HttpServletRequest request, HttpServletResponse response){
+		ModelAndView view = new ModelAndView("/test");
+
+		return view;
 	}
 }
