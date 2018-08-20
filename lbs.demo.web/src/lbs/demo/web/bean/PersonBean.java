ad686a6fd7888b36aa28f5d3e8d540ba8f3d5699
Lenovo <Lenovo@DESKTOP-670EF3R>
2018-08-20 17:01:31 +0900

    PersonBean

---
+++ lbs.demo.web/src/lbs/demo/web/bean/PersonBean.java
@@ -0,0 +1,35 @@
+package lbs.demo.web.bean;
+
+public class PersonBean {
+
+	private String id;
+
+	private String name;
+
+	private String country;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+}
